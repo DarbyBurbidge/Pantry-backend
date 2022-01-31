091054c0d6c14b4d70dbbea435558146bcb304af
DarbyBurbidge <darby.burbidge@gmail.com>
2022-01-31 03:34:45 -0800

    How is this not working Lost my .env file needed to fix a command setup build and production run commands

---
+++ dist/index.js
@@ -0,0 +1,38 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const server_1 = __importDefault(require("./server"));
+const user_resolver_1 = require("./resolvers/user.resolver");
+const category_resolver_1 = require("./resolvers/category.resolver");
+const item_resolver_1 = require("./resolvers/item.resolver");
+dotenv_1.default.config();
+mongoose_1.default.connect(`${process.env.DB_URI}`, async (err) => {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+});
+const db = mongoose_1.default.connection;
+db.on('error', console.error.bind(console, 'Connection Error'));
+db.once('open', async () => {
+    console.log("connected to MongoDB Atlas");
+});
+(async () => {
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: await (0, type_graphql_1.buildSchema)({
+            resolvers: [user_resolver_1.UserResolver, category_resolver_1.CategoryResolver, item_resolver_1.ItemResolver]
+        }),
+        context: ({ req, res }) => ({ req, res })
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app: server_1.default, cors: false });
+    server_1.default.listen(process.env.PORT, () => { console.log(`Listening on port ${process.env.PORT}`); });
+})();
+//# sourceMappingURL=index.js.map
---
+++ dist/resolvers/item.resolver.js
@@ -0,0 +1,108 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ItemResolver = void 0;
+const typegoose_1 = require("@typegoose/typegoose");
+const item_model_1 = require("../models/item.model");
+const type_graphql_1 = require("type-graphql");
+const isauth_middleware_1 = require("../middleware/isauth.middleware");
+const returnObject_resolver_1 = require("./returnObject.resolver");
+const generateDate = (date) => {
+    if (date == 'N/A') {
+        return date;
+    }
+    const seperated = date.split('-');
+    const day = parseInt(seperated[2]);
+    const month = parseInt(seperated[1]);
+    const year = seperated[0].substring(seperated.length - 1);
+    return `${month}/${day}/${year}`;
+};
+let ItemResolver = class ItemResolver {
+    async getItems() {
+        return await (0, typegoose_1.getModelForClass)(item_model_1.Item).find();
+    }
+    async addItem(itemName, categoryId, expiration, { payload }) {
+        try {
+            const date = generateDate(expiration);
+            await (0, typegoose_1.getModelForClass)(item_model_1.Item).create({ categoryId: categoryId, userId: payload === null || payload === void 0 ? void 0 : payload.userId, itemName: itemName, expiration: date });
+        }
+        catch (err) {
+            console.error(err);
+            return { message: `${err}`, return: false };
+        }
+        return { message: "OK", return: true };
+    }
+    async editItem(_id, itemName, expiration, { payload }) {
+        try {
+            const date = generateDate(expiration);
+            await (0, typegoose_1.getModelForClass)(item_model_1.Item).findOneAndUpdate({ _id: _id, userId: payload === null || payload === void 0 ? void 0 : payload.userId }, { itemName: itemName, expiration: date });
+        }
+        catch (err) {
+            console.log(err);
+            return { message: `${err}`, return: false };
+        }
+        return { message: "OK", return: true };
+    }
+    async deleteItem(_id) {
+        try {
+            await (0, typegoose_1.getModelForClass)(item_model_1.Item).findByIdAndDelete(_id);
+        }
+        catch (err) {
+            console.error(err);
+            return { message: `${err}`, return: false };
+        }
+        return { message: "OK", return: true };
+    }
+};
+__decorate([
+    (0, type_graphql_1.Query)(() => [item_model_1.Item], { nullable: true }),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", Promise)
+], ItemResolver.prototype, "getItems", null);
+__decorate([
+    (0, type_graphql_1.Mutation)(() => returnObject_resolver_1.ReturnObject),
+    (0, type_graphql_1.UseMiddleware)(isauth_middleware_1.isAuth),
+    __param(0, (0, type_graphql_1.Arg)('itemName')),
+    __param(1, (0, type_graphql_1.Arg)('categoryId')),
+    __param(2, (0, type_graphql_1.Arg)('expiration')),
+    __param(3, (0, type_graphql_1.Ctx)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, String, String, Object]),
+    __metadata("design:returntype", Promise)
+], ItemResolver.prototype, "addItem", null);
+__decorate([
+    (0, type_graphql_1.Mutation)(() => returnObject_resolver_1.ReturnObject),
+    (0, type_graphql_1.UseMiddleware)(isauth_middleware_1.isAuth),
+    __param(0, (0, type_graphql_1.Arg)('_id')),
+    __param(1, (0, type_graphql_1.Arg)('itemName')),
+    __param(2, (0, type_graphql_1.Arg)('expiration')),
+    __param(3, (0, type_graphql_1.Ctx)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, String, String, Object]),
+    __metadata("design:returntype", Promise)
+], ItemResolver.prototype, "editItem", null);
+__decorate([
+    (0, type_graphql_1.Mutation)(() => returnObject_resolver_1.ReturnObject),
+    (0, type_graphql_1.UseMiddleware)(isauth_middleware_1.isAuth),
+    __param(0, (0, type_graphql_1.Arg)('_id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", Promise)
+], ItemResolver.prototype, "deleteItem", null);
+ItemResolver = __decorate([
+    (0, type_graphql_1.Resolver)()
+], ItemResolver);
+exports.ItemResolver = ItemResolver;
+//# sourceMappingURL=item.resolver.js.map
