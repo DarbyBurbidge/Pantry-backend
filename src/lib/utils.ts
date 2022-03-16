5895e32f9919d5538b85b793bbcb88dcd0c80e2f
DarbyBurbidge <darby.burbidge@gmail.com>
2022-03-15 18:21:33 -0700

    changed Item resolver to take the parents type and use it to findByIdAndUpdate. Unresolved errors still exist

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,10 @@
+import { getModelForClass, mongoose } from "@typegoose/typegoose";
 import crypto from "crypto-js"
+import { ShoppingList } from "../models/shoppingList.model";
+import { User } from "../models/user.model";
 
 
-
+/* Password Utils */
 export const verifyPassword = (password: string, salt: string, hash: string) => {
     const genHash = crypto.PBKDF2(password, salt, {
         keySize: 64,
@@ -24,6 +27,8 @@ export const hashPassword = (password: string) => {
     };
 }
 
+
+/* Date Utils */
 export const generateDate = (date: string) => {
     if (date == 'N/A') {
         return date
@@ -34,3 +39,44 @@ export const generateDate = (date: string) => {
     const year = seperated[0].substring(seperated.length - 1);
     return `${month}/${day}/${year}`
 }
+
+
+/* Resolver Utils */
+interface ObjectLiteral {
+  [key: string]: any;
+}
+
+
+export const deleteFromParent: ObjectLiteral = {
+    'user': async (_id: mongoose.Types.ObjectId) => {
+        await getModelForClass(User).findOneAndUpdate({$in: {itemIds: _id}}, {$pull: {itemIds: _id}});
+    },
+    'list': async (_id: mongoose.Types.ObjectId) => {
+        await getModelForClass(ShoppingList).findOneAndUpdate({$in: {itemIds: _id}}, {$pull: {itemIds: _id}});
+    },
+    'default': async () => {
+        throw new Error("Unable to locate parent");
+    }
+};
+
+export const addToParent: ObjectLiteral = {
+    'user': async (parentId: string, _id: string) => {
+        try {
+            await getModelForClass(User).findByIdAndUpdate(parentId, { $addToSet: { itemIds: _id } });
+        } catch (err) {
+            console.error(err)
+            throw new Error(err)
+        }
+    },
+    'list': async (parentId: string, _id: string) => {
+        try {
+            await getModelForClass(ShoppingList).findByIdAndUpdate(parentId, { $addToSet: { itemIds: _id } });
+        } catch (err) {
+            console.error(err);
+            throw new Error(err)
+        }
+    },
+    'default': () => {
+        throw new Error("No parent specified");
+    }
+};
--- src/resolvers/item.resolver.ts
+++ src/resolvers/item.resolver.ts
@@ -4,8 +4,7 @@ import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware } from "type-graphql
 import { AppContext } from "../context/app.context";
 import { isAuth } from "../middleware/isauth.middleware";
 import { ReturnObject } from "./returnObject.resolver";
-import { User } from "../models/user.model";
-import { generateDate } from "../lib/utils";
+import { addToParent, deleteFromParent, generateDate } from "../lib/utils";
 
 
 
@@ -17,6 +16,9 @@ export class ItemResolver {
         return await getModelForClass(Item).find()
     }
 
+    /* ADD ITEM */
+
+    // Add to User
     @Mutation(() => ReturnObject)
     @UseMiddleware(isAuth)
     async addItem(
@@ -24,12 +26,56 @@ export class ItemResolver {
         @Arg('expiration') expiration: string,
         @Arg('quantity') quantity: number,
         @Arg('tags', () => [String]) tags: string[],
-        @Ctx() { payload }: AppContext
+        @Arg('parentType') parentType: string,
+        @Arg('parentId') parentId: string,
+        //@Ctx() { payload }: AppContext
     ) {
         try { 
+            const date = generateDate(expiration);
+            const item = await getModelForClass(Item).create({itemName: itemName, expiration: date, quantity: quantity, tags: tags});
+            //await getModelForClass(User).findByIdAndUpdate(payload?.userId,{ $addToSet: {itemIds: item._id}});
+            (addToParent[parentType] || addToParent['default'])(parentId, String(item._id));
+        } catch (err) {
+            console.error(err)
+            return {message: `${err}`, return: false}
+        }
+        return {message: "OK", return: true}
+    }
+/*
+    // Add to ShoppingList
+    @Mutation(() => ReturnObject)
+    @UseMiddleware(isAuth)
+    async addListItem(
+        @Arg('itemName') itemName: string,
+        @Arg('expiration') expiration: string,
+        @Arg('quantity') quantity: number,
+        @Arg('tags', () => [String]) tags: string[],
+        @Arg('listId') listId: string
+    ) {
+        try {
             const date = generateDate(expiration)
-            const item = await getModelForClass(Item).create({itemName: itemName, expiration: date, quantity: quantity, tags: tags})
-            await getModelForClass(User).findByIdAndUpdate(payload?.userId,{ $addToSet: {itemIds: item._id}})
+            const item = await getModelForClass(Item).create({ itemName: itemName, expiration: date, quantity: quantity, tags: tags })
+            await getModelForClass(ShoppingList).findByIdAndUpdate(listId, { $addToSet: { itemIds: item._id } })
+
+        } catch (err) {
+            console.error(err)
+            return { message: `${err}`, return: false }
+        }
+        return { message: "OK", return: true }
+    }
+*/
+    /* Delete Item */
+
+    // Delete from User
+    @Mutation(() => ReturnObject)
+    @UseMiddleware(isAuth)
+    async deleteItem(
+        @Arg('id') id: string,
+        @Arg('parentType') parentType: string
+    ) {
+        try {
+            (deleteFromParent[parentType] || deleteFromParent['default'])(id);
+            await getModelForClass(Item).findByIdAndDelete(id);
         } catch (err) {
             console.error(err)
             return {message: `${err}`, return: false}
@@ -37,6 +83,27 @@ export class ItemResolver {
         return {message: "OK", return: true}
     }
 
+/*    // Delete from ShoppingList
+    @Mutation(() => ReturnObject)
+    @UseMiddleware(isAuth)
+    async deleteListItem(
+        @Arg('_id') _id: string,
+    ) {
+        try {
+            await getModelForClass(ShoppingList).findOneAndUpdate({ $in: { itemIds: _id } }, { $pull: { itemIds: _id } })
+            await getModelForClass(Item).findByIdAndDelete(_id)
+        } catch (err) {
+            console.error(err)
+            return { message: `${err}`, return: false }
+        }
+        return { message: "OK", return: true }
+    }
+*/
+
+
+    /* EDIT ITEM */
+
+    // Edit is Parent Neutral
     @Mutation(() => ReturnObject)
     @UseMiddleware(isAuth)
     async editItem(
@@ -55,20 +122,4 @@ export class ItemResolver {
         }
         return {message: "OK", return: true}
     }
-
-    @Mutation(() => ReturnObject)
-    @UseMiddleware(isAuth)
-    async deleteItem(
-        @Arg('_id') _id: string,
-    ) {
-        try {
-            await getModelForClass(User).findOneAndUpdate({$in: {itemIds: _id}}, {$pull: {itemIds: _id}})
-            await getModelForClass(Item).findByIdAndDelete(_id)
-        } catch (err) {
-            console.error(err)
-            return {message: `${err}`, return: false}
-        }
-        return {message: "OK", return: true}
-    }
-
 }
